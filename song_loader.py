fb9b43f5148451c2ee4b7004f8343ad29dacde85
Eddie Kubit <eddiekubit0@gmail.com>
2024-01-30 10:09:50 -0500

    Simple native dataset load and query

--- song_loader.py
+++ song_loader.py
@@ -7,3 +7,23 @@ elastic_pwd = elastic_pwd_file.read()
 elastic_pwd_file.close()
 
 es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=("elastic", elastic_pwd))
+
+# kaggle datasets download -d joebeachcaptial/30000-spotify-songs
+
+with open("init_songs.json") as file:
+    songs = json.loads(file.read())
+
+if not es.indices.exists(index='songs'):
+    print('Created blank index "songs"')
+    es.indices.create(index='songs')
+
+for song in songs:
+    response = es.search(index='songs', body={"query": {"match_phrase": {"track_name": song['track_name']}}})
+    if response['hits']['total']['value'] == 0:
+        es.index(index='songs',body = song)
+
+
+response = es.search(index='songs', body={"query": {"match": {"track_name": "Dance Monkey"}}})
+print(response)
+
+
