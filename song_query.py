c72f16a6fd2cf5435de752b111aa6b530a4db7ee
Eddie Kubit <eddiekubit0@gmail.com>
2024-02-01 13:59:42 -0500

    Five most similar songs to Macarena on danceability and genre

--- song_query.py
+++ song_query.py
@@ -9,11 +9,32 @@ elastic_pwd_file.close()
 
 es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=("elastic", elastic_pwd))
 
+def searchSimilar(targetSongData):
+    targetSubGenre = targetSongData['playlist_subgenre']
+    targetDanceability = targetSongData['danceability']
+    similarSongs = es.search(index='songs', body={'query': {'bool': {'must':[{'match': {'playlist_subgenre' : targetSubGenre}}, {'match':{'danceability': targetDanceability}}]}}})
+    return similarSongs['hits']['hits']
+
+def queryTop10(standInParams):
+    trackname = standInParams['track_name']
+    targetSong = es.search(index='songs', body={"query": {"match": {"track_name": trackname}}})['hits']['hits'][0]
+    targetSongData = targetSong['_source']
+
+    names = []
+    similarSongs = searchSimilar(targetSongData)
+    for song in similarSongs:
+        names.append(song['_source']['track_name'])
+    return names
+    
+
 app = Flask(__name__)
 
 @app.route('/query')
 def getSong():
-    response = es.search(index='songs', body={"query": {"match": {"track_name": "Dance Monkey"}}})
+    standInParams = dict()
+    standInParams['track_name'] = 'Macarena'
+    return queryTop10(standInParams)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print(getSong())
+    #app.run(debug=True)
