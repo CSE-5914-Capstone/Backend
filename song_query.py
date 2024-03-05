02fad0722b674f92338156ee3c4b806748f07b14
Eddie Kubit <eddiekubit0@gmail.com>
2024-03-05 10:18:32 -0500

    stopped tracking unique songs

--- song_query.py
+++ song_query.py
@@ -1,9 +1,10 @@
 from elasticsearch import Elasticsearch, helpers
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 import os
 import json
 import song_loader
+import normalize_parameters
 
 elastic_pwd_file = open('docker_elastic_pwd.txt', 'r')
 elastic_pwd = elastic_pwd_file.read()
@@ -13,17 +14,13 @@ es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=
 
 def searchSimilar(targetSongData):
     #Some of the following variables are subject to change
-    # targetSubGenre = targetSongData['playlist_subgenre']
+
     targetDanceability = targetSongData['danceability']
-    # targetGenre = targetSongData['playlist_genre']
     targetEnergy = targetSongData['energy']
-    # targetKey = targetSongData['key']
     targetLoudness = targetSongData['loudness']
     targetLiveness = targetSongData['liveness']
     targetHappiness = targetSongData['valence']
     targetTempo = targetSongData['tempo']
-    #{'match': {'playlist_subgenre' : targetSubGenre}}
-    #{'match':{'key' : targetKey}}
     similarSongs = es.search(index='songs', body={'query': {'bool': { #'must': [{'match':{'playlist_genre' : targetGenre}}],
                     'filter': [
                     {'range':{'danceability': {'gte': targetDanceability - 0.15, 'lte': targetDanceability + 0.15}}}, 
@@ -54,8 +51,11 @@ CORS(app)
 
 @app.route('/query')
 def getSong():
+    track_name = request.args.get('trackname')
+    if track_name is None:
+        track_name = "Macarena"
     standInParams = dict()
-    standInParams['track_name'] = 'Beat It'
+    standInParams['track_name'] = f"{track_name}"
     return queryTop10(standInParams)
 
 @app.route('/')
